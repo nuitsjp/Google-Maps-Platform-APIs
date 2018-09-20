5faa7036546540ce73a493d8d5326119baae4626
nuits.jp <nuits.jp@live.jp>
2018-09-20 23:44:05 +0900

    Add Project.

---
+++ Nuits.Google.Apis.Places/Nuits.Google.Apis.Places/PlacesApi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Nuits.Google.Apis.Places
+{
+    public class PlacesApi
+    {
+        public PlacesApi(string appSecret)
+        {
+            AppSecret = appSecret;
+        }
+
+        private static HttpClient Client { get; } = new HttpClient { BaseAddress = new Uri("https://maps.googleapis.com/maps/api/place/") };
+
+        private string AppSecret { get; }
+
+        public void NearbySearchAsync()
+        {
+            var uri = $"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=35.681167,139.767052&radius=1500&type=restaurant&key={AppSecret}";
+        }
+
+    }
+}
